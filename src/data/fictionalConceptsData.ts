a21894445706771eaf3a2f63a0726400772d34fb
Wissam <wissamheldis@gmail.com>
2026-06-20 19:35:13 +0200

    fix: sanitize image paths for production

--- src/data/fictionalConceptsData.ts
+++ src/data/fictionalConceptsData.ts
@@ -13,12 +13,12 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'tesla',
     brandName: 'TESLA',
     materials: ['Aluminium', 'Cuivre', 'Graphite', 'Lithium'],
-    coverPath: '/materials paysage + palette matiere/Cuivre/GRIFZ x TESLA02_4K.png',
+    coverPath: '/materials-paysage/Cuivre/GRIFZ x TESLA02_4K.png',
     galleryPaths: [
-      '/materials paysage + palette matiere/Cuivre/GRIFZ x TESLA05_4K.png',
-      '/materials paysage + palette matiere/Aluminium/GRIFZ x TESLA18_4K.png',
-      '/materials paysage + palette matiere/Graphite/GRIFZ x TESLA24_4K.png',
-      '/materials paysage + palette matiere/Lithium/GRIFZ x TESLA25_4K.png'
+      '/materials-paysage/Cuivre/GRIFZ x TESLA05_4K.png',
+      '/materials-paysage/Aluminium/GRIFZ x TESLA18_4K.png',
+      '/materials-paysage/Graphite/GRIFZ x TESLA24_4K.png',
+      '/materials-paysage/Lithium/GRIFZ x TESLA25_4K.png'
     ],
     rainColor: '#b87333' ,
     logoPath: '/works/logo brands/tesla.png'// Cuivre
@@ -27,11 +27,11 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'apple',
     brandName: 'APPLE',
     materials: ['Gallium', 'Tungstène'],
-    coverPath: '/materials paysage + palette matiere/Gallium/GRIFZ x APPLE11_4K.png',
+    coverPath: '/materials-paysage/Gallium/GRIFZ x APPLE11_4K.png',
     galleryPaths: [
-      '/materials paysage + palette matiere/Gallium/GRIFZ x APPLE14_4K.png',
-      '/materials paysage + palette matiere/Tungstene/GRIFZ x APPLE17_4K.png',
-      '/materials paysage + palette matiere/Tungstene/GRIFZ x APPLE18_4K.png'
+      '/materials-paysage/Gallium/GRIFZ x APPLE14_4K.png',
+      '/materials-paysage/Tungstene/GRIFZ x APPLE17_4K.png',
+      '/materials-paysage/Tungstene/GRIFZ x APPLE18_4K.png'
     ],
     rainColor: '#d3d3d3' ,
     logoPath: '/works/logo brands/apple.png'// Gris métal/Gallium
@@ -40,11 +40,11 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'lvmh',
     brandName: 'LVMH',
     materials: ['Cuir Classic', 'Cuir Rouge', 'Cuir Vert', 'Cuir Beige'],
-    coverPath: '/materials paysage + palette matiere/Cuir ( variantes )/GRIFZ x LVMH02_4K.png',
+    coverPath: '/materials-paysage/Cuir-variantes/GRIFZ x LVMH02_4K.png',
     galleryPaths: [
-      '/materials paysage + palette matiere/Cuir ( variantes )/Cuir Rouge/GRIFZ x LVMH06_4K.png',
-      '/materials paysage + palette matiere/Cuir ( variantes )/Cuir vert/GRIFZ x LVMH07_4K.png',
-      '/materials paysage + palette matiere/Cuir ( variantes )/Cuire beige/GRIFZ x LVMH08_4K.png'
+      '/materials-paysage/Cuir-variantes/Cuir Rouge/GRIFZ x LVMH06_4K.png',
+      '/materials-paysage/Cuir-variantes/Cuir vert/GRIFZ x LVMH07_4K.png',
+      '/materials-paysage/Cuir-variantes/Cuire beige/GRIFZ x LVMH08_4K.png'
     ],
     rainColor: '#8b4513' ,
     logoPath: '/works/logo brands/LVMH.png'// Cuir SaddleBrown
@@ -53,10 +53,10 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'toyota',
     brandName: 'TOYOTA',
     materials: ['Charbon', 'Chrome', 'Palladium'],
-    coverPath: '/materials paysage + palette matiere/Charbon/GRIFZ x TOYOTA08_4K.png',
+    coverPath: '/materials-paysage/Charbon/GRIFZ x TOYOTA08_4K.png',
     galleryPaths: [
-      '/materials paysage + palette matiere/Palladium/GRIFZ x TOYOTA19_4K.png',
-      '/materials paysage + palette matiere/Chrome/GRIFZ x TOYOTA24_4K.png'
+      '/materials-paysage/Palladium/GRIFZ x TOYOTA19_4K.png',
+      '/materials-paysage/Chrome/GRIFZ x TOYOTA24_4K.png'
     ],
     rainColor: '#36454f' ,
     logoPath: '/works/logo brands/toyota.png'// Charbon
@@ -65,13 +65,13 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'samsung',
     brandName: 'SAMSUNG',
     materials: ['Indium'],
-    coverPath: '/materials paysage + palette matiere/Indium/GRIFZ x SAMSUNG01_4K.png',
+    coverPath: '/materials-paysage/Indium/GRIFZ x SAMSUNG01_4K.png',
     galleryPaths: [
-      '/materials paysage + palette matiere/Indium/GRIFZ x SAMSUNG02_4K.png',
-      '/materials paysage + palette matiere/Indium/GRIFZ x SAMSUNG03_4K.png',
-      '/materials paysage + palette matiere/Indium/GRIFZ x SAMSUNG04_4K.png',
-      '/materials paysage + palette matiere/Indium/GRIFZ x SAMSUNG05_4K.png',
-      '/materials paysage + palette matiere/Indium/GRIFZ x SAMSUNG06_4K.png'
+      '/materials-paysage/Indium/GRIFZ x SAMSUNG02_4K.png',
+      '/materials-paysage/Indium/GRIFZ x SAMSUNG03_4K.png',
+      '/materials-paysage/Indium/GRIFZ x SAMSUNG04_4K.png',
+      '/materials-paysage/Indium/GRIFZ x SAMSUNG05_4K.png',
+      '/materials-paysage/Indium/GRIFZ x SAMSUNG06_4K.png'
     ],
     rainColor: '#0057b7' ,
     logoPath: '/works/logo brands/Samsung.png'// Samsung Blue (Indium ref)
@@ -80,7 +80,7 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'pepsico',
     brandName: 'PEPSI',
     materials: ['Céréales'],
-    coverPath: '/materials paysage + palette matiere/Cereales/GRIFZ x PEPSI.co04_4K.png',
+    coverPath: '/materials-paysage/Cereales/GRIFZ x PEPSI.co04_4K.png',
     galleryPaths: [],
     rainColor: '#e5ca9a' ,
     logoPath: '/works/logo brands/pepsi.png'// Céréales
@@ -89,7 +89,7 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'coca-cola',
     brandName: 'COCA COLA',
     materials: ['PET'],
-    coverPath: '/materials paysage + palette matiere/PET/GRIFZ x COCA COLA04_4K.png',
+    coverPath: '/materials-paysage/PET/GRIFZ x COCA COLA04_4K.png',
     galleryPaths: [],
     rainColor: '#ff0000' ,
     logoPath: '/works/logo brands/cocacola.png'// Rouge
@@ -98,7 +98,7 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'michelin',
     brandName: 'MICHELIN',
     materials: ['Caoutchouc naturel'],
-    coverPath: '/materials paysage + palette matiere/Caoutchouc naturel/GRIFZ x MICHELIN03_4K.png',
+    coverPath: '/materials-paysage/Caoutchouc naturel/GRIFZ x MICHELIN03_4K.png',
     galleryPaths: [],
     rainColor: '#2b2b2b' ,
     logoPath: '/works/logo brands/michelin.png'// Caoutchouc
@@ -107,7 +107,7 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'ikea',
     brandName: 'IKEA',
     materials: ['Bois'],
-    coverPath: '/materials paysage + palette matiere/Bois/GRIFZ x IKEA02_4K.png',
+    coverPath: '/materials-paysage/Bois/GRIFZ x IKEA02_4K.png',
     galleryPaths: [],
     rainColor: '#deb887' ,
     logoPath: '/works/logo brands/ikea.png'// Bois
@@ -116,7 +116,7 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'zara',
     brandName: 'ZARA',
     materials: ['Coton'],
-    coverPath: '/materials paysage + palette matiere/Coton/GRIFZ x ZARA04_4K.png',
+    coverPath: '/materials-paysage/Coton/GRIFZ x ZARA04_4K.png',
     galleryPaths: [],
     rainColor: '#ffffff' ,
     logoPath: '/works/logo brands/zara.png'// Coton
@@ -125,7 +125,7 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'leroy-merlin',
     brandName: 'LEROY MERLIN',
     materials: ['PVC'],
-    coverPath: '/materials paysage + palette matiere/PVC/GRIFZ x LEROY MERLIN03_4K.png',
+    coverPath: '/materials-paysage/PVC/GRIFZ x LEROY MERLIN03_4K.png',
     galleryPaths: [],
     rainColor: '#7db928' ,
     logoPath: '/works/logo brands/leroy merlin.png'// Vert Leroy Merlin
@@ -134,7 +134,7 @@ export const fictionalConcepts: FictionalConcept[] = [
     id: 'boeing',
     brandName: 'BOEING',
     materials: ['Titane'],
-    coverPath: '/materials paysage + palette matiere/Titane/GRIFZ x BOEING02_4K.png',
+    coverPath: '/materials-paysage/Titane/GRIFZ x BOEING02_4K.png',
     galleryPaths: [],
     rainColor: '#878681' ,
     logoPath: '/works/logo brands/boeing.png'// Titane
